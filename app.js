9a40441c45f22056b59268ad1b5e66b6490d231f
jimilbean <bcshko20@naver.com>
2019-09-23 15:37:12 +0900

    0923수업끝

---
+++ app.js
@@ -0,0 +1,20 @@
+Number.prototype.timeFormat = function(){
+    let h = "0" + Math.floor(this / 3600);
+    h = h.substring(h.length - 2, h.length);
+    let m = "0" + Math.floor(this % 3600 / 60);
+    m = m.substring(m.length - 2, m.length);
+    let s = "0" + Math.floor(this % 60);
+    s = s.substring(s.length - 2, s.length);
+    return `${h} : ${m} : ${s}`;
+}
+
+class App{
+    constructor(playerEL, listEL){
+        this.player = new Player(playerEL, this);
+        this.playList = new PlayList(listEL, this);
+    }
+}
+
+window.addEventListener("load", ()=>{
+    let app = new App("#player", "#playList");
+});
---
+++ playList.js
@@ -0,0 +1,107 @@
+class PlayList {
+    constructor(el, app){
+        this.app = app;
+        this.listDom = document.querySelector(el);
+        this.itemList = this.listDom.querySelector(".item-list");
+        this.addBtn = this.listDom.querySelector("#openDialog");
+        this.fileInput = this.listDom.querySelector("#audioFile");
+
+        this.itemList.innerHTML = "";
+        this.fileList = []; //플레이리스트 상에 있는 음악파일들을 저장
+        this.playIdx = null; //현재 재생중인 음악의 인덱스를 저장
+
+        this.currentMusic = null;
+
+        this.contextMenu = document.querySelector("#context");
+        this.contextTargetItem = null;
+
+        this.addListener();
+    }
+
+    addListener(){
+        this.addBtn.addEventListener("click", e => this.fileInput.click());
+        this.fileInput.addEventListener("change", this.inputChange.bind(this));
+
+        this.listDom.addEventListener("dragover", this.fileDragOver.bind(this));
+        this.listDom.addEventListener("drop", this.fileDrop.bind(this));
+
+
+        this.contextMenu.querySelector("#del").addEventListener("click", (e)=>{
+            console.log(this.contextTargetItem);
+            e.stopPropagation();
+        });
+
+        document.querySelector("body").addEventListener("click", (e)=> {
+            this.contextMenu.style.visibility = "hidden";
+            this.contextTargetItem = null;
+        });
+    }
+
+    fileDragOver(e){
+        e.preventDefault();
+        e.stopPropagation();
+    }
+
+    fileDrop(e){
+        e.preventDefault();
+        e.stopPropagation();
+        let files = Array.from(e.dataTransfer.files);
+        this.addList(files);
+
+    }
+
+    inputChange(e){
+        let files = Array.from(e.target.files);
+        this.addList(files);
+
+    }
+
+
+
+    addList(files) {
+        files.forEach(file => {
+            if(file.type.substring(0, 5) !== "audio") return;
+            let obj = {idx: this.fileList.length, file: file, dom: null};
+            this.fileList.push(obj);
+            let item = document.createElement("li");
+            item.classList.add("item");
+            obj.dom = item;
+            item.addEventListener("dblclick", (e) => {
+                let data = this.fileList.find(x => x.idx == obj.idx);
+                this.playItem(data);
+            });
+
+            item.addEventListener("contextmenu", (e)=> {
+                e.preventDefault();
+                e.stopPropagation();
+                this.contextTargetItem = obj;
+                this.contextMenu.style.top = e.pageY + "px";
+                this.contextMenu.style.left = e.pageX + "px";
+                this.contextMenu.style.visibility = "visible";
+            });
+
+            item.innerHTML = file.name;
+            this.itemList.appendChild(item);
+        });
+    }
+
+    playItem(data){
+        this.fileList.forEach(file => {
+            file.dom.classList.remove("active");
+        });
+
+        this.currentMusic = data.idx;  //현재 재생중인 음악의 idx를 저장
+
+        data.dom.classList.add("active");
+        this.app.player.loadMusic(data.file);
+    }
+
+    getNextMusic(loop){
+        let now = this.fileList.findIndex(x => x.idx == this.currentMusic);
+        if(now < this.fileList.length - 1){
+            this.playItem(this.fileList[now + 1]);
+        }else if(loop){
+            this.playItem(this.fileList[0]);
+        }
+    }
+}
